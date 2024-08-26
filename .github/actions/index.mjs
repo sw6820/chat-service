cf3ddfa95b6de02b851bd2583c5cbf77877c7c42
sw6820 <5320shin@hanmail.net>
2024-08-26 17:39:15 +0900

    REFACTOR: add .js

--- .github/actions/index.mjs
+++ .github/actions/index.mjs
@@ -1,5 +1,5 @@
-import * as core from '@actions/core';
-import * as exec from '@actions/exec';
+import * as core from '@actions/core.js';
+import * as exec from '@actions/exec.js';
 
 async function run() {
   try {
