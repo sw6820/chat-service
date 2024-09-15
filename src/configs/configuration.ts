419a959f144005487baa5840b5e28a46c11970a1
sw6820 <5320shin@hanmail.net>
2024-09-15 16:57:39 +0900

    CHORE: unused config is removed

--- src/configs/configuration.ts
+++ src/configs/configuration.ts
@@ -1,38 +1,38 @@
 import { readFileSync } from 'fs';
-import * as yaml from 'js-yaml';
+// import * as yaml from 'js-yaml';
 import * as dotenv from 'dotenv';
 import common from './common';
-import dev from './dev';
-import local from './local';
-import prod from './prod';
-import { Config } from '../types/config.types';
+// import dev from './dev';
+// import local from './local';
+// import prod from './prod';
+// import { Config } from '../types/config.types';
 
 const phase = process.env.NODE_ENV as 'local' | 'dev' | 'prod';
 // console.log(`Phase: ${phase}`);
 
 dotenv.config({ path: `./envs/.env.${phase}` });
 
-const YAML_CONFIG_FILENAME = 'config.yaml';
+// const YAML_CONFIG_FILENAME = 'config.yaml';
 
-const configs: Record<'local' | 'dev' | 'prod', Config> = {
-  local: local,
-  dev: dev,
-  prod: prod,
-};
+// const configs: Record<'local' | 'dev' | 'prod', Config> = {
+//   local: local,
+//   dev: dev,
+//   prod: prod,
+// };
+//
+// const conf: any = configs[phase] || {};
+//
+// // Read and preprocess the YAML file to replace environment variables
+// const yamlContent = readFileSync(
+//   `${process.cwd()}/envs/${YAML_CONFIG_FILENAME}`,
+//   'utf8',
+// );
+// const yamlWithEnv = yamlContent.replace(
+//   /\${(\w+)}/g,
+//   (_, variable) => process.env[variable] || '',
+// );
 
-const conf: any = configs[phase] || {};
-
-// Read and preprocess the YAML file to replace environment variables
-const yamlContent = readFileSync(
-  `${process.cwd()}/envs/${YAML_CONFIG_FILENAME}`,
-  'utf8',
-);
-const yamlWithEnv = yamlContent.replace(
-  /\${(\w+)}/g,
-  (_, variable) => process.env[variable] || '',
-);
-
-const yamlConfig: Record<string, any> = yaml.load(yamlWithEnv);
+// const yamlConfig: Record<string, any> = yaml.load(yamlWithEnv);
 
 // const yamlConfig: Record<string, any> = yaml.load(
 //   readFileSync(`${process.cwd()}/envs/${YAML_CONFIG_FILENAME}`, 'utf8'),
@@ -42,5 +42,5 @@ const yamlConfig: Record<string, any> = yaml.load(yamlWithEnv);
 export default () => ({
   ...common, // ./common.ts
   // ...conf, // ./${}.ts
-  ...yamlConfig, // ./envs/config.yaml
+  // ...yamlConfig, // ./envs/config.yaml
 });
