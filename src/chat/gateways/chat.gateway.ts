3db37e970bd82177aa1cec8d619a516e28755d18
sw6820 <5320shin@hanmail.net>
2024-12-12 23:30:59 +0900

    FIX: cors origin

--- src/chat/gateways/chat.gateway.ts
+++ src/chat/gateways/chat.gateway.ts
@@ -20,7 +20,8 @@ import { JwtService } from '@nestjs/jwt';
     // origin:
     // '*',
     origin: [
-      'https://1479d390.chat-service-frontend.pages.dev/',
+      'https://*.chat-service-frontend.pages.dev',
+      'https://chat-service-frontend.pages.dev',
       'http://localhost:3000',
       'http://localhost:8080',
       'http://127.0.0.1:8080',
